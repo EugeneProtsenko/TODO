1559c46ad082a1404c6609f3e9bdad6b2d7db259
Eugene Protsenko <guitaaarpro@gmail.com>
2024-03-17 12:34:38 +0100

    modifing templates, wich related to tag page

--- todo/urls.py
+++ todo/urls.py
@@ -1,14 +1,35 @@
 from django.urls import path
 
-from todo.views import TodoListView
+from todo.views import TodoListView, TagListView, TagUpdateView, TagDeleteView, TagCreateView
 
 app_name = "todo"
 
 
 urlpatterns = [
     path(
-        "todos/",
+        "",
         TodoListView.as_view(),
         name="todo-list",
     ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list",
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create",
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete",
+    ),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from todo.models import Todo
+from todo.models import Todo, Tag
 
 
 class TodoListView(generic.ListView):
@@ -9,3 +10,27 @@ class TodoListView(generic.ListView):
     context_object_name = "todo_list"
     template_name = "todo/todo_list.html"
     # paginate_by = 5
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+    template_name = "todo/tag_list.html"
+    # paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("taxi:manufacturer-list")
